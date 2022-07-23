7655f5a81588cef5706c38f2c426e1ea05cb7aeb
Muqiu Han <muqiu-han@outlook.com>
2022-07-23 22:36:39 +0800

    Add some common procedures that will be used by other components.

---
+++ src/utils.rkt
@@ -0,0 +1,52 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; utils.rkt : This file will contain common procedures that will be used by other components. ;;
+;;                                                                                             ;;
+;; Copyright (c) 2022 Muqiu Han                                                                ;;
+;;                                                                                             ;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy                ;;
+;; of this software and associated documentation files (the "Software"), to deal               ;;
+;; in the Software without restriction, including without limitation the rights                ;;
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                   ;;
+;; copies of the Software, and to permit persons to whom the Software is                       ;;
+;; furnished to do so, subject to the following conditions:                                    ;;
+;;                                                                                             ;;
+;; The above copyright notice and this permission notice shall be included in all              ;;
+;; copies or substantial portions of the Software.                                             ;;
+;;                                                                                             ;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                  ;;
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                    ;;
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                 ;;
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                      ;;
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,               ;;
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE               ;;
+;; SOFTWARE.                                                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+#lang racket/base
+
+(require racket/serialize)
+
+;; Return true if a predicate satisfies all members of the list
+(define (utils/true-for-all? pred list)
+  (cond
+    [(null? list) #t]
+    [(pred (car list)) (utils/true-for-all? pred (cdr list))]
+    [else #f]))
+
+;; Export a struct into a file
+(define (utils/struct->file object file)
+  (let ([out (open-output-file file #:exists 'replace)])
+    (write (serialize object) out)
+    (close-output-port out)))
+
+;; Return the struct by openning the file, reading its contents, and deserializing its contents.
+(define (utils/file->struct file)
+  (letrec ([in (open-input-file file)]
+           [result (read in)])
+    (close-input-port in)
+    (deserialize result)))
+
+
+(provide utils/true-for-all?
+         utils/file->struct
+         utils/struct->file)
