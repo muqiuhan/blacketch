de86143808b7b58c0536d6627bd47766ac8a9a4a
Muqiu Han <muqiu-han@outlook.com>
2022-07-23 18:17:07 +0800

    add the necessary procedures to create and mine the block

--- src/block.rkt
+++ src/block.rkt
@@ -1,7 +1,13 @@
 #lang racket/base
 
-(require sha)
+(require (only-in openssl/sha1 hex-string->bytes))
+(require (only-in sha sha256))
+(require (only-in sha bytes->hex-string))
 (require racket/serialize)
+
+(define difficulty 2)
+(define target (bytes->hex-string
+                (make-bytes difficulty 32)))
 
 ;; A block should contain the current hash, the previous hash, the data and the timestamp that it was generate
 ;; Using a hashing algorithm will allow us to confirm that the block is valid.
@@ -25,11 +31,40 @@
      (string->bytes/utf-8 (format "~a" (serialize transaction)))
      (string->bytes/utf-8 (number->string nonce))))))
 
+
 ;; Verify block, just hash the block's contents again and compare this hash to the one stored in the block.
-(define (valid-block? a-block)
+(define (block/valid? a-block)
   (equal? (block-current-hash a-block)
           (block/calculate-hash
            (block-previous-hash a-block)
            (block-timestamp a-block)
            (block-transaction a-block)
            (block-nonce a-block))))
+
+;; A block will be considered mined if the hash matches the target, given the difficulty
+(define (block/mined? block-hash)
+  (equal? (subbytes
+           (hex-string->bytes block-hash)
+           1 difficulty)
+          (subbytes
+           (hex-string->bytes target)
+           1 difficulty)))
+
+;; Hashcrash
+;; This procedure keeps increasing the nonce until a block is valid, at which point it is returned.
+;; That is, we continuously change the nonce until sha256 produces a hash that matches the target.
+;; This defines the foundations of mining (proof of work).
+(define (block/make-and-mine previous-hash timestamp transaction nonce)
+  (let ([current-hash (block/calculate-hash previous-hash timestamp transaction nonce)])
+    (if (block/mined? current-hash)
+        (block current-hash previous-hash transaction timestamp nonce)
+        (block/make-and-mine previous-hash timestamp transaction (+ nonce 1)))))
+
+;; Help procedure
+(define (block/mine transaction previous-hash)
+  (block/make-and-mine previous-hash (current-milliseconds) transaction 1))
+
+(provide block/valid?
+         block/mined?
+         block/mine
+         (struct-out block))
