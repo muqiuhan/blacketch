a02fcc4b1ab354b89ff4eba04973165956ed0ac1
Muqiu Han <muqiu-han@outlook.com>
2022-07-23 16:20:21 +0800

    INIT

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "blacketch")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/blacketch.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(Muqiu Han))
+(define license '(MIT))
---
+++ scribblings/blacketch.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[blacketch
+                    racket/base]]
+
+@title{blacketch}
+@author{muqiuhan}
+
+@defmodule[blacketch]
+
+Package Description Here
