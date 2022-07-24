c2785dfe1c23f268f85e3544234686d5c54d6f80
Muqiu Han <muqiu-han@outlook.com>
2022-07-24 17:36:30 +0800

    2022-07-24 5:36

--- src/wallet.rkt
+++ src/wallet.rkt
@@ -27,19 +27,19 @@
 (require crypto)
 (require crypto/all)
 
-;; A wallet is structure that contains a public and a private key.
 (struct wallet
   (private-key public-key)
   #:prefab)
 
-;; Create a wallet by generating random public and private keys, and it will rely on the RSA algorithm.
-(define (wallet/create)
+; Make wallet by generating random public and private keys.
+(define (wallet/make)
   (letrec ([rsa-impl (get-pk 'rsa libcrypto-factory)]
-           [prikey (generate-private-key rsa-impl '((nbits 512)))]
-           [pubkey (pk-key->public-only-key prikey)])
-    (wallet
-     (bytes->hex-string (pk-key->datum prikey 'PrivateKeyInfo))
-     (bytes->hex-string (pk-key->datum pubkey 'SubjectPublicKeyInfo)))))
+           [privkey (generate-private-key rsa-impl '((nbits 512)))]
+           [pubkey (pk-key->public-only-key privkey)])
+    (wallet (bytes->hex-string
+             (pk-key->datum privkey 'PrivateKeyInfo))
+            (bytes->hex-string
+             (pk-key->datum pubkey 'SubjectPublicKeyInfo)))))
 
 (provide (struct-out wallet)
-         wallet/create)
+         wallet/make)
